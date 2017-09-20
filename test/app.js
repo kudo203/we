0f6bfa54404208c75e7e9d0601b17de476f7f169
Koosh Doctor <koosh.20@gmail.com>
2017-09-20 01:43:21 -0400

    server username and password update

--- test/app.js
+++ test/app.js
@@ -5,11 +5,11 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
+    if(process.env.MONGODB_USERNAME) { // check if running remotely
+        var username = process.env.MONGODB_USERNAME; // get from environment
+        var password = process.env.MONGODB_PASSWORD;
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString += '@ds141474.mlab.com:41474/heroku_qr3ss1t7'; // user yours
     }
 
     var mongoose = require("mongoose");
